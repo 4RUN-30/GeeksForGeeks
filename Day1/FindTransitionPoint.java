420120cb4e5833dc9f5f0a9f92098f2c705c1711
4RUN-30 <102476267+4RUN-30@users.noreply.github.com>
2023-11-04 06:19:30 +0530

    Update FindTransitionPoint.java

--- Day1/FindTransitionPoint.java
+++ Day1/FindTransitionPoint.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    int transitionPoint(int arr[], int n) {
+        if(arr[n-1]==0) return -1;
+        if(arr[0]==1) return 0;
+        return findPos(arr,0,n-1);
+    }
+    int findPos(int[] arr, int start, int end){
+        
+        int mid = (start + end)/2;
+        if(mid > 0 && arr[mid]==1 && arr[mid-1]==0){
+            return mid;
+        }
+        if(arr[mid] == 0){
+            // move right
+            return findPos(arr,mid+1,end);
+        }
+        if(arr[mid] == 1){
+            return findPos(arr,start,mid-1);
+        }
+        return -1;
+    }
+}
